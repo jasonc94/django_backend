fd515e2b2797a1e724935f043958d69f2a725681
Jason Cai <jasoncai528@gmail.com>
2024-10-25 12:55:52 -0700

    add separate url for users

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -23,7 +23,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("polls/", include("polls.urls")),
-    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/user/", include("users.urls")),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
+    path("api-auth/", include("rest_framework.urls")),
 ]
