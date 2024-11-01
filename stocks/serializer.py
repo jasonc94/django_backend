85905748eec32baeb125929ee0fa1775ed3e3a4c
Jason Cai <jasoncai528@gmail.com>
2024-11-01 15:12:26 -0700

    add serializer and transactionlist create view

---
+++ stocks/serializer.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from stocks.models import Transaction
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = (
+            "user",
+            "stock",
+            "transaction_type",
+            "quantity",
+            "price_per_share",
+            "date",
+        )
+        read_only_fields = ("user", "date")
+
+
+class PortfolioSerializer(serializers.Serializer):
+    ticker = serializers.CharField()
+    company_name = serializers.CharField()
+    total_quantity = serializers.FloatField()
+    average_cost = serializers.DecimalField(max_digits=10, decimal_places=2)
+    total_amount = serializers.DecimalField(max_digits=10, decimal_places=2)
--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.db import transaction
+from rest_framework.generics import CreateAPIView, ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from stocks.models import Holding, Transaction
+from stocks.serializer import TransactionSerializer
 
 # Create your views here.
+
+
+class TransactionListCreate(ListCreateAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [AllowAny]
+    # permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        all_transactions = Transaction.objects.all()
+        return all_transactions
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            transaction_instance = serializer.save(user=self.request.user)
+
+            if transaction_instance.transaction_type == "buy":
+                Holding.objects.create(
+                    user=self.request.user,
+                    stock=transaction_instance.stock,
+                    quantity=transaction_instance.quantity,
+                    cost=transaction_instance.price_per_share,
+                    date=transaction_instance.date,
+                )
