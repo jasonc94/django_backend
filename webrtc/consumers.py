f12fda1c99f6214f355099e29a4e27e2f3ac0fba
Jason Cai <jasoncai528@gmail.com>
2025-03-22 19:09:38 -0700

    add sender as part of query connection

--- webrtc/consumers.py
+++ webrtc/consumers.py
@@ -5,11 +5,28 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class WebRTCConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_group_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.userId = self.scope["url_route"]["kwargs"]["userId"]
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        # await self.channel_layer.group_send(
+        #     self.room_group_name,
+        #     {
+        #         "type": "broadcast_message",
+        #         "payload": {"type": "join", "userId": self.userId},
+        #         "sender": self.userId,
+        #     },
+        # )
         await self.accept()
 
     async def disconnect(self, close_code):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "broadcast_message",
+                "payload": {"type": "leave", "userId": self.userId},
+                "sender": self.userId,
+            },
+        )
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
@@ -19,11 +36,13 @@ class WebRTCConsumer(AsyncWebsocketConsumer):
             {
                 "type": "broadcast_message",
                 "payload": data,
-                "sender_channel": self.channel_name,
+                "sender": self.userId,
             },
         )
 
     async def broadcast_message(self, event):
-        if event["sender_channel"] == self.channel_name:
+        if event["sender"] == self.userId:
             return
-        await self.send(text_data=json.dumps(event["payload"]))
+        await self.send(
+            text_data=json.dumps({"sender": event["sender"], **event["payload"]})
+        )
--- webrtc/routing.py
+++ webrtc/routing.py
@@ -4,6 +4,7 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(
-        r"ws/squad-connect/(?P<room_name>\w+)/$", consumers.WebRTCConsumer.as_asgi()
+        r"ws/squad-connect/(?P<room_name>\w+)/(?P<userId>\w+)/$",
+        consumers.WebRTCConsumer.as_asgi(),
     ),
 ]
