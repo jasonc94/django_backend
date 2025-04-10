fc71005514a2961773f30059615ae9d633ced5b2
Jason Cai <jasoncai528@gmail.com>
2025-04-09 20:51:56 -0700

    fix serializer

--- chatrooms/serializer.py
+++ chatrooms/serializer.py
@@ -1,14 +1,15 @@
 from chatrooms.models import Room, Participant
+from rest_framework import serializers
 
 
-class RoomSerializer:
+class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ["id", "name", "createdAt"]
         extra_kwargs = {"createdAt": {"read_only": True}}
 
 
-class ParticipantSerializer:
+class ParticipantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participant
         fields = ["id", "name", "joinedAt", "leftAt"]
