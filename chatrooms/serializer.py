4b298c0a44557a9a68cddc5893d94685a6ae233a
Jason Cai <jasoncai528@gmail.com>
2025-04-09 21:38:22 -0700

    include participants

--- chatrooms/serializer.py
+++ chatrooms/serializer.py
@@ -2,14 +2,16 @@ from chatrooms.models import Room, Participant
 from rest_framework import serializers
 
 
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        fields = ["id", "name", "createdAt"]
-        extra_kwargs = {"createdAt": {"read_only": True}}
-
-
 class ParticipantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participant
         fields = ["id", "name", "joinedAt", "leftAt"]
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    participants = ParticipantSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Room
+        fields = ["id", "name", "createdAt", "participants"]
+        extra_kwargs = {"createdAt": {"read_only": True}}
