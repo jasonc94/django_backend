ab4cf1bec6311b7c2a7e82e3c637a61444df13e1
Jason Cai <jasoncai528@gmail.com>
2025-04-09 21:38:55 -0700

    only include active participants with prefetch

--- chatrooms/views.py
+++ chatrooms/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets
 from chatrooms.serializer import RoomSerializer
-from .models import Room
+from .models import Participant, Room
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.db import models
 
 
 # Create your views here.
 class RoomViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
-    queryset = Room.objects.all()
+    queryset = Room.objects.prefetch_related(
+        models.Prefetch(
+            "participants", queryset=Participant.objects.filter(leftAt__isnull=True)
+        )
+    )
     serializer_class = RoomSerializer
